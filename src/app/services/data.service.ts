889d2575fd2f5843b05f56be43ba03693caa1ce1
TomSygiel <tomasz.szczygiel@medieinstitutet.se>
2019-04-30 10:44:17 +0200

    Add the getData function and api to data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IDataService } from '../interfaces/IDataService';
+import { Observable } from 'rxjs';
+import { IMovieProducts } from '../interfaces/IMovieProducts';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataService {
+export class DataService implements IDataService {
 
-  constructor() { }
+  getData(): Observable<IMovieProducts[]> {
+    return this.http.get<IMovieProducts[]>('https://medieinstitutet-wie-products.azurewebsites.net/api/products');
+  }
+
+  constructor(private http: HttpClient) { }
 }
