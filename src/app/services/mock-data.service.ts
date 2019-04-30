582e18c529a6d5548992209719ad0e16d71e5528
TomSygiel <tomasz.szczygiel@medieinstitutet.se>
2019-04-30 10:32:52 +0200

    Add mock data to mock data service

--- src/app/services/mock-data.service.ts
+++ src/app/services/mock-data.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
 import { IDataService } from '../interfaces/IDataService';
+import { Observable, of } from 'rxjs';
+import { IMovieProducts } from '../interfaces/IMovieProducts';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MockDataService implements IDataService {
+
+  movies: IMovieProducts[] = [
+    {id: 1, name: 'Movie One', price: 100, imageUrl: 'https://bit.ly/2Wgcmpx', description: 'Really a good movie!'},
+    {id: 2, name: 'Movie Two', price: 150, imageUrl: 'https://bit.ly/2Wgcmpx', description: 'Really a great movie!'},
+    {id: 3, name: 'Movie Three', price: 200, imageUrl: 'https://bit.ly/2Wgcmpx', description: 'Really a best movie!'}
+  ]
+
+  getData(): Observable<IMovieProducts[]> {
+    return of(this.movies);
+  }
 
   constructor() { }
 }
