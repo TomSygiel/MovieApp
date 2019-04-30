d7a2c9804aacf9a8ec5f2f04c2bb0d8b0cade44a
TomSygiel <tomasz.szczygiel@medieinstitutet.se>
2019-04-30 13:39:43 +0200

    Create home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { IMovieProducts } from '../interfaces/IMovieProducts';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  movies: IMovieProducts[];
+
+  constructor(private service: DataService) { }
+
+  ngOnInit() {
+    this.service.getData().subscribe(
+      movieList => this.movies = movieList
+    );
+  }
+
+}
