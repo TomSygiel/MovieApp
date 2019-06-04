4cc3860c395ab49daf6f0900c13237e6be5ed87a
TomSygiel <tomasz.szczygiel@medieinstitutet.se>
2019-06-04 15:49:05 +0200

    Add modal for pop up message window

---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit {
+
+  @Input() show = false;
+  @Input() customClass = '';
+  @Input() closeCallback = () => (false);
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
