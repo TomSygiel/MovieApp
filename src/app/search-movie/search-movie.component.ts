69b9b9b09e3b4002da823d115dd5f4f8064bc75e
TomSygiel <tomasz.szczygiel@medieinstitutet.se>
2019-06-07 12:16:05 +0200

    Add changes to search movie API and to function in both movie filter pipe file and search movie .ts file

--- src/app/search-movie/search-movie.component.ts
+++ src/app/search-movie/search-movie.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IMovieProducts, ICategorySelector } from '../interfaces/IMovieProducts';
 import { DataService } from '../services/data.service';
-import { FormControl } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-
 
 @Component({
   selector: 'app-search-movie',
@@ -15,10 +12,6 @@ export class SearchMovieComponent implements OnInit {
   movies: IMovieProducts[];
   filteredMovies: IMovieProducts[];
   categories: ICategorySelector[];
-
-  //Code for searchQuery
-  // results: any[] = [];
-  // searchQuery: FormControl = new FormControl();
 
   //Code for searchTerm and NgModel
   private _searchTerm: string;
@@ -32,60 +25,35 @@ export class SearchMovieComponent implements OnInit {
   }
 
   searchForMovieTitle(searchString: string) {
-    return this.movies.filter(movie => 
-      movie.name.toLowerCase().indexOf(searchString.toLowerCase()) !== 1);
+    return this.filteredMovies.filter(movie => 
+      movie.name.toLowerCase().indexOf(searchString.toLowerCase()) >= 0);
   }
 
-  constructor(private service: DataService, private http: HttpClient) { 
-    http.get<IMovieProducts[]>('https://medieinstitutet-wie-products.azurewebsites.net/api/search').subscribe(result => {
-      this.movies = result;
-      this.filteredMovies = result;
-    });
+  // reset(searchString: string) {
+  //   this.searchTerm.reset(searchString);
+  //   }
+  // }
+
+  constructor(private service: DataService) { 
+  
   }
 
   ngOnInit() {
 
-    // this.movies = this.service.getData();
-
-    this.filteredMovies = this.movies;
-
-    //Code for searchQuery
-    // this.searchQuery.valueChanges.subscribe( result => {
-    //   console.log(result);
-    // });
--
-    // this.searchQuery.valueChanges
-    // .subscribe(searchQuery => this.service.getMovieSearchResult(searchQuery)
-    // .subscribe(movieSearchResult => {
-    //     this.movies = movieSearchResult
-    //     console.log(this.movies);
-    // }));
-
-     //Show all movie selection
+    //Show all movie selection
     this.service.getData().subscribe(movieList => {
         this.filteredMovies = movieList
         console.log(this.filteredMovies);
     });
+
+    //Filter result
+    this.filteredMovies = this.movies;
 
     this.service.getCategories().subscribe(data => {
       this.categories = data;
     });
 
   }
-
-  // searchForMovieTitle() {
-    // this.service.getMovieSearchResult().subscribe(movieSearchResult => {
-    //   this.movies = movieSearchResult
-    //   console.log(this.movies);
-    // });
-
-    // this.searchQuery.valueChanges
-    // .subscribe(searchQuery => this.service.getMovieSearchResult(searchQuery)
-    // .subscribe(movieSearchResult => {
-    //     this.movies = movieSearchResult
-    //     console.log(this.movies);
-    // }));
-  // }
 
   openNav() {
     document.getElementById("mySidebar").style.width = "275px";
