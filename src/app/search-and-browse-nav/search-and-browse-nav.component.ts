9552176ba562e0aca69122629c7e2a48772cab8a
TomSygiel <tomasz.szczygiel@medieinstitutet.se>
2019-06-11 13:47:14 +0200

    Add nav bar that implements both Search and Browse links

---
+++ src/app/search-and-browse-nav/search-and-browse-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-and-browse-nav',
+  templateUrl: './search-and-browse-nav.component.html',
+  styleUrls: ['./search-and-browse-nav.component.css']
+})
+export class SearchAndBrowseNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
